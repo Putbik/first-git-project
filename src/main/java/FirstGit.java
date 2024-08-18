54f28fa63417593581fb12b137010fc54a52049c
Putbik <ledovskei@gmail.com>
2024-08-18 13:59:12 +0300

    Add sout

--- src/main/java/FirstGit.java
+++ src/main/java/FirstGit.java
@@ -2,5 +2,7 @@ public class FirstGit {
     public static void main(String[] args) {
         System.out.println("GIT");
         System.out.println("LEARNING!");
+        System.out.println("It's terribly difficult");
+
     }
 }
