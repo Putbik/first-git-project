38e40204f39b45e21688a28b915831c59d32c8b0
Putbik <ledovskei@gmail.com>
2024-08-18 14:21:07 +0300

    change sout

--- src/main/java/FirstGit.java
+++ src/main/java/FirstGit.java
@@ -2,7 +2,7 @@ public class FirstGit {
     public static void main(String[] args) {
         System.out.println("GIT DEVELOP");
         System.out.println("LEARNING!");
-        System.out.println("It's terribly difficult");
+        System.out.println("It's terribly difficult very");
          System.out.println("I'm happy");
         System.out.println("!!!");
     }
